4e2c4b9c6a37f3523e074d96da60bbd6597c39f3
wnsh3131@naver.com <wnsh3131@naver.com>
2021-07-11 16:32:13 +0900

    Daliy Algorithm

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,8 +1,15 @@
 ﻿// Algorithm.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
+#include "maxSumSubmatrix.h"
 
 int main() {
+
+	vector<vector<int>> test = { {1,0,1},{0,-2,3} };
+	int k = 2;
+
+	Solution s;
+	s.maxSumSubmatrix(test, k);
 
 	return 1;
 }
--- Algorithm/lastStoneWeight.cpp
+++ Algorithm/lastStoneWeight.cpp
@@ -1,20 +1,18 @@
 #include "algorithmHeader.h"
 
-class Solution {
-    int lastStoneWeight(vector<int>& stones) {
-        std::priority_queue<int, std::vector<int>, std::greater<int>> pq(begin(stones), end(stones));
+ int lastStoneWeight(vector<int>& stones) {
+     std::priority_queue<int, std::vector<int>, std::greater<int>> pq(begin(stones), end(stones));
 
-        while (pq.size() > 1) {
-            int x = pq.top();
-            pq.pop();
-            int y = pq.top();
-            pq.pop();
+     while (pq.size() > 1) {
+         int x = pq.top();
+         pq.pop();
+         int y = pq.top();
+         pq.pop();
 
-            int temp = x - y;
-            if (temp > 0) {
-                pq.push(temp);
-            }
-        }
-        return pq.empty() ? 0 : pq.top();
-    }
-};
+         int temp = x - y;
+         if (temp > 0) {
+             pq.push(temp);
+         }
+     }
+     return pq.empty() ? 0 : pq.top();
+ }
